01214c15f05a1bf716c7678c2176f2a778042103
Jeremy Tsang <jeremy@jeremytsang.com>
2020-03-03 10:58:55 -0800

    Implement and test method for algebraic strings
    Make method to translate the required algebraic strings into
    conventional row and column values.

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -4,4 +4,18 @@
 
 
 class XiangqiGame:
-    pass
+    # Class level constants
+    ALPHABET_DCT = {letter: i for i, letter in enumerate('abcdefghi')}
+
+    @staticmethod
+    def alg_to_row_col(alg_str):
+        # TODO: Add exception handling for
+        #   - OOB
+        #   - short strings
+        #   - long strings
+        #   - invalid chars
+        alg_letter = alg_str[0]
+        alg_num = int(alg_str[1:])  # TODO: wrap in try block
+        print(f'(alg_letter, alg_num): {alg_letter}, {alg_num}')
+
+        return (alg_num - 1, XiangqiGame.ALPHABET_DCT[alg_letter])

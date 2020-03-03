610a3311114f28d22594a9d7f3fd584aecb1c464
Jeremy Tsang <jeremy@jeremytsang.com>
2020-03-03 12:20:28 -0800

    Define exception classes for algebraic notation

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -38,3 +38,41 @@ class XiangqiGame:
     @staticmethod
     def get_ALPHABET_DCT():
         return XiangqiGame._ALPHABET_DCT
+
+
+class Error(Exception):
+    """Base class for all exceptions."""
+    def __init__(self, message):
+        Exception.__init__(self, message)
+
+
+class AlgStrFormattingError(Error):
+    """Base exception class for all expcetions relating to the Algebraic
+    notation errors."""
+    pass
+
+
+class AlgStrLengthError(AlgStrFormattingError):
+    def __init__(self):
+        max_row_digits = len(str(XiangqiGame._ROW_COUNT))
+        super().__init__('Algebraic string must be between 2 to '
+                         + f'{1 + max_row_digits} '
+                         + 'characters long (inclusive).')
+
+
+class AlgLetterError(AlgStrFormattingError):
+    def __init__(self):
+        letters = XiangqiGame.get_ALPHABET()[:XiangqiGame.get_COL_COUNT()]
+        super().__init__(f'Algebraic column letter must be in "{letters}".')
+
+
+class AlgNumFormatError(AlgStrFormattingError):
+    def __init__(self):
+        super().__init__('Algebraic row number must be a valid integer.')
+
+
+class AlgNumOutOfBoundsError(AlgStrFormattingError):
+    def __init__(self):
+        super().__init__('Algebraic row number must fall between 1 and '
+                         + f'{XiangqiGame.get_ROW_COUNT()} '
+                         + 'inclusive.')

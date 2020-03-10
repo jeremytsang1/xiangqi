0cc72cdc55067a0662b0d8a4d1aabfc0189277ef
Jeremy Tsang <jeremy@jeremytsang.com>
2020-03-09 19:05:11 -0700

    Add comments to init test for XiangqiGame

--- test_portfolio.py
+++ test_portfolio.py
@@ -5,10 +5,13 @@ import XiangqiGame as xg
 class XiangqiGameTest(unittest.TestCase):
     def test_init(self):
         game = xg.XiangqiGame()
+        # Make sure only two players.
         self.assertEqual(2, len(game.get_players()))
+        # Make sure game is not already over when created.
         self.assertEqual("UNFINISHED", game.get_game_state())
         red = game.get_players()[xg.Player.get_RED()]
         black = game.get_players()[xg.Player.get_BLACK()]
+        # Check opponent tracking.
         self.assertEqual(black, red.get_opponent())
         self.assertEqual(red, black.get_opponent())
 

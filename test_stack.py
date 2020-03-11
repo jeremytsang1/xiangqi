b565afe62b2b34bcb517fa496e4cab739dcf74fc
Jeremy Tsang <jeremy@jeremytsang.com>
2020-03-10 19:42:28 -0700

    Add integration test for class Stack

--- test_stack.py
+++ test_stack.py
@@ -22,3 +22,20 @@ class StackTest(unittest.TestCase):
             for param in params:
                 stack.push(param)
             self.assertEqual(top, stack.peek())
+
+    def test_push_and_pop(self):
+        test_cases = (
+            (2, 7, 12, 78, 23),
+            tuple(),
+            (0, 0, 0, 0),
+            (65,),
+        )
+        for params in test_cases:
+            stack = xg.Stack()
+            for param in params:
+                stack.push(param)
+            self.assertEqual(len(params), stack.get_size())
+            for i, param in enumerate(params[::-1]):
+                top = stack.pop()
+                self.assertEqual(param, top)
+            self.assertEqual(0, stack.get_size())

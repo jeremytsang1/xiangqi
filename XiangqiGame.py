b18a0429e46efa8c918165bf571119cbee5890e3
Jeremy Tsang <jeremy@jeremytsang.com>
2020-03-10 19:50:22 -0700

    Move class Stack to XiangqiGame.py
    Move all tests to test_portfolio.py.

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -565,6 +565,38 @@ class Player:
         return Player._HOME_ROW
 
 
+class Stack:
+    """
+    An implementation of the Stack ADT that uses Python's built-in lists.
+
+    Taken from CS 162, "Exploration: Linked lists, stacks, queues".
+    """
+    def __init__(self):
+        self._list = []
+
+    def get_size(self):
+        return len(self._list)
+
+    def peek(self):
+        if self.get_size() == 0:
+            return None
+        return self._list[-1]
+
+    def is_empty(self):
+        return len(self._list) == 0
+
+    def push(self, data):
+        self._list.append(data)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        val = self._list[-1]
+        del self._list[-1]
+        return val
+
+
 class AlgNot:
     """Class to handle the board's Algebraic notation positional reference."""
     _ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
--- test_portfolio.py
+++ test_portfolio.py
@@ -292,3 +292,42 @@ class PlayerTest(unittest.TestCase):
                 expected = count
                 actual = len(pieces[key])
                 self.assertEqual(expected, actual)
+
+
+class StackTest(unittest.TestCase):
+    def test_init(self):
+        stack = xg.Stack()
+        self.assertEqual(0, stack.get_size())
+        self.assertEqual(None, stack.peek())
+        self.assertTrue(stack.is_empty())
+        self.assertEqual(None, stack.pop())
+
+    def test_peek_push_from_empty(self):
+        test_cases = {
+            (2,): 2,
+            (2, 5): 5,
+            (2, 5, 3): 3,
+            (2, 2, 2, 2): 2,
+        }
+        for params, top in test_cases.items():
+            stack = xg.Stack()
+            for param in params:
+                stack.push(param)
+            self.assertEqual(top, stack.peek())
+
+    def test_push_and_pop(self):
+        test_cases = (
+            (2, 7, 12, 78, 23),
+            tuple(),
+            (0, 0, 0, 0),
+            (65,),
+        )
+        for params in test_cases:
+            stack = xg.Stack()
+            for param in params:
+                stack.push(param)
+            self.assertEqual(len(params), stack.get_size())
+            for i, param in enumerate(params[::-1]):
+                top = stack.pop()
+                self.assertEqual(param, top)
+            self.assertEqual(0, stack.get_size())
